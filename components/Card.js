f382a3f02a0bd0b6ba171857e6cf2a7efb4eb3f0
oliver leal <oliver.lealch@gmail.com>
2023-07-27 01:39:26 -0300

    alteraciones css card sola

--- components/Card.js
+++ components/Card.js
@@ -1,32 +1,27 @@
-import Link from "next/link"
+import Link from "next/link";
 
-const Card = ({user}) => {
-    console.log(user)
+const Card = ({ user }) => {
+  console.log(user);
 
-    return (
-        <Link href={`users/${user.id}`}>
-            <div className="cajap">
-            <div className="box">
-                
-                  <img src={`http://robohash.org/${user.id}`}
-                   alt="robot-pic"
-                    style={{width: "200px", height: "200px"}}></img>
-                    
-                    <div className="text">
-                    <h3>Name: {user.name}</h3>
-                    <p>Email: {user.email}</p>
-                    <p>Cellphone: {user.phone}</p>
-                    </div>
-            </div>
-            </div>
+  return (
+    <Link href={`users/${user.id}`}>
+      <div className="cajap">
+        <div className="box">
+          <img
+            src={`http://robohash.org/${user.id}`}
+            alt="robot-pic"
+            style={{ width: "200px", height: "200px" }}
+          ></img>
 
-        </Link>
-    )
-}
+          <div className="text">
+            <h3>Name: {user.name}</h3>
+            <p>Email: {user.email}</p>
+            <p>Cellphone: {user.phone}</p>
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+};
 
-
-
-
-
-
-export default Card
+export default Card;
